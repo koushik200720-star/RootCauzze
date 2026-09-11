df002f1daa7ac2b532283262f3088aba1c3133b4
koushik200720-star <koushik200720@gmail.com>
2026-09-11 23:55:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -19,6 +19,10 @@ db.init_app(app)
 app.register_blueprint(auth_bp)
 app.register_blueprint(machine_bp)
 
+print("\n===== REGISTERED ROUTES =====")
+print(app.url_map)
+print("=============================\n")
+
 # Create database tables
 with app.app_context():
     db.create_all()
